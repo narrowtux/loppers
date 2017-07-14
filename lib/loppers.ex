6b388afec7b8995a2070540084c94143f303e7bd
Moritz Schmale <narrow.m@gmail.com>
2017-07-14 11:49:10 +0200

    mix new

---
+++ lib/loppers.ex
@@ -0,0 +1,18 @@
+defmodule Loppers do
+  @moduledoc """
+  Documentation for Loppers.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Loppers.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/loppers_test.exs
@@ -0,0 +1,8 @@
+defmodule LoppersTest do
+  use ExUnit.Case
+  doctest Loppers
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
